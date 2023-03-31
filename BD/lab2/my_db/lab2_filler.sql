1e275154dad146dacf4dcf4e04624c398024253e
mmonastyrskiy <56514528+mmonastyrskiy@users.noreply.github.com>
2023-03-31 14:31:21 +0300

    Update lab2_filler.sql

--- BD/lab2/my_db/lab2_filler.sql
+++ BD/lab2/my_db/lab2_filler.sql
@@ -6,30 +6,35 @@ TRUNCATE "C21-703-7"."Shelf" CASCADE;
 TRUNCATE "C21-703-7"."product" CASCADE;
 
 SET Datestyle to German;
+CREATE SEQUENCE room_generator INCREMENT BY 1 CACHE 10 START 1;
+CREATE SEQUENCE shelf_generator INCREMENT BY 1 CACHE 10 START 1;
+CREATE SEQUENCE client_generator INCREMENT BY 1 CACHE 10 START 100;
+CREATE SEQUENCE contract_generator INCREMENT BY 1 CACHE 10 START 1;
+CREATE SEQUENCE pid_generator INCREMENT BY 1 CACHE 10 START 1;
 
 INSERT INTO "C21-703-7"."Room" Values
-(1,'Room1',50,50,20,60,18,32),
-(2,'Room2',500,500,40,100,22,32),
-(3,'Room3',50,50,20,60,18,32),
-(10,'Garage1',10000,10000,40,100,22,32),
-(4,'Room4',50,50,20,60,18,32),
-(5,'Room5',500,500,40,100,22,32);
-INSERT INTO "C21-703-7"."Shelf" Values
-(1,1,10,10,500,400,300,500,500),
-(2,1,50,50,500,400,400,600,600),
-(3,2,40,40,500,500,500,500,500),
-(4,3,40,40,500,500,500,500,500),
-(5,4,5,5,500,500,500,40,40);
+(nextval('room_generator'),'Room1',50,50,20,60,18,32),
+(nextval('room_generator'),'Room2',500,500,40,100,22,32),
+(nextval('room_generator'),'Room3',50,50,20,60,18,32),
+(nextval('room_generator'),'Garage1',10000,10000,40,100,22,32),
+(nextval('room_generator'),'Room4',50,50,20,60,18,32);
 
-INSERT INTO "C21-703-7"."Client" Values(100,'PAO SBERBANK',
+INSERT INTO "C21-703-7"."Shelf" Values
+(nextval('shelf_generator'),(select id from "C21-703-7"."Room" where name ='Room1'),10,10,500,400,300,500,500),
+(nextval('shelf_generator'),(select id from "C21-703-7"."Room" where name ='Room1'),50,50,500,400,400,600,600),
+(nextval('shelf_generator'),(select id from "C21-703-7"."Room" where name ='Room2'),40,40,500,500,500,500,500),
+(nextval('shelf_generator'),(select id from "C21-703-7"."Room" where name ='Room3'),40,40,500,500,500,500,500),
+(nextval('shelf_generator'),(select id from "C21-703-7"."Room" where name ='Room4'),5,5,500,500,500,40,40);
+
+INSERT INTO "C21-703-7"."Client" Values(nextval('client_generator'),'PAO SBERBANK',
 'Юридический адрес	117312, г. Москва, ул. Вавилова, д.19
 БИК	044525225
 ИНН	7707083893
 КПП	773643001
 ОКПО	57972160'),
-(101,' Российская Государственная Библиотека',
+(nextval('client_generator'),' Российская Государственная Библиотека',
 ' ИНН 7704097560, ОГРН 1037739390809, ОКПО 02175175'),
-(102,'Мираторг', 'Наименование:	"Агропромышленный холдинг "Мираторг", ООО
+(nextval('client_generator'),'Мираторг', 'Наименование:	"Агропромышленный холдинг "Мираторг", ООО
 Основной ОКВЭД:	Консультирование по вопросам коммерческой деятельности и управления
 Страна:	РОССИЯ
 Регион:	Московская область
@@ -38,7 +43,7 @@ INSERT INTO "C21-703-7"."Client" Values(100,'PAO SBERBANK',
 Данные госрегистрации:	№1077763208874 от 29.11.2007, Межрайонная инспекция ФНС N 46 по г. Москве
 Юридический адрес:	142030, Московская обл, г. Домодедово, тер. Трио-Инвест-Ям (Центральный Мкр, стр. 3
 Почтовый адрес:	142000, Московская обл., г. Домодедово, мкр Центральный, тер. Трио-Инвест-Ям, стр. 3'),
-(103,'НИЯУ МИФИ', 'Федеральное государственное автономное учреждение
+(nextval('client_generator'),'НИЯУ МИФИ', 'Федеральное государственное автономное учреждение
 Полное наименование:
 Федеральное государственное автономное образовательное учреждение высшего образования «Национальный исследовательский ядерный университет «МИФИ»
 Наименование без ОПФ:
@@ -67,7 +72,7 @@ MEPhI
 Адрес электронной почты:
 info@mephi.ru'),
 
-(104,'ОЗОН','Юридический адрес	123112, г. Москва, Пресненская наб., д. 10, эт. 41, Пом. I, комн. 6
+(nextval('client_generator'),'ОЗОН','Юридический адрес	123112, г. Москва, Пресненская наб., д. 10, эт. 41, Пом. I, комн. 6
 Фактический адрес	123112, Москва, Пресненская наб., д. 10, блок С, комплекс «Башня на набережной»
 № ОГРН	1027739244741
 ИНН	7704217370
@@ -84,17 +89,31 @@ info@mephi.ru'),
 БИК	044525700');
 
 
-INSERT INTO "C21-703-7"."Contract" Values(1,'25-03-24','26-03-23',100);
-INSERT INTO "C21-703-7"."Contract" Values(2,'25-03-24','26-03-23',100);
 
-INSERT INTO "C21-703-7"."Contract" Values(3,'29-03-24','30-03-23',101);
-INSERT INTO "C21-703-7"."Contract" Values(4,'29-03-24','30-03-23',102);
-INSERT INTO "C21-703-7"."Contract" Values(5,'29-03-24','30-03-23',103);
-INSERT INTO "C21-703-7"."Contract" Values(6,'29-03-24','30-03-23',104);
+CREATE OR REPLACE FUNCTION slot_finder(ushelf_id integer) RETURNS integer as $$
+BEGIN
+CASE when (SELECT count(*) from "C21-703-7"."product" p where p.shelf_id = ushelf_id) > 0 then
+RETURN (SELECT max(slot_id) + 1 FROM "C21-703-7"."product" p where p.shelf_id = ushelf_id);
+else 
+RETURN 1;
+END CASE;
+END;
 
-INSERT INTO "C21-703-7"."product" Values(1,110,120,130,now(),1,60,30,20,50,5,2,10);
-INSERT INTO "C21-703-7"."product" Values(2,300,300,300,now(),2,60,30,20,50,1,3,100);
-INSERT INTO "C21-703-7"."product" Values(3,210,220,230,now(),3,80,50,40,70,2,1,37);
-INSERT INTO "C21-703-7"."product" Values(4,70,50,30,now(),4,60,30,20,50,5,3,10);
-INSERT INTO "C21-703-7"."product" Values(5,110,120,130,now(),5,60,30,20,50,5,6,70);
+$$ language plpgsql;
+
+
+
+INSERT INTO "C21-703-7"."Contract" Values(nextval('contract_generator'),'25-03-24','26-03-23',(SELECT client_id FROM "C21-703-7"."Client" where name = 'PAO SBERBANK'));
+INSERT INTO "C21-703-7"."Contract" Values(nextval('contract_generator'),'25-03-24','26-03-23',(SELECT client_id FROM "C21-703-7"."Client" where name = 'PAO SBERBANK'));
+
+INSERT INTO "C21-703-7"."Contract" Values(nextval('contract_generator'),'29-03-24','30-03-23',(SELECT client_id FROM "C21-703-7"."Client" where name = 'ОЗОН'));
+INSERT INTO "C21-703-7"."Contract" Values(nextval('contract_generator'),'29-03-24','30-03-23',(SELECT client_id FROM "C21-703-7"."Client" where name = 'НИЯУ МИФИ'));
+INSERT INTO "C21-703-7"."Contract" Values(nextval('contract_generator'),'29-03-24','30-03-23',(SELECT client_id FROM "C21-703-7"."Client" where name = 'НИЯУ МИФИ'));
+INSERT INTO "C21-703-7"."Contract" Values(nextval('contract_generator'),'29-03-24','30-03-23',(SELECT client_id FROM "C21-703-7"."Client" where name = 'Мираторг'));
+
+INSERT INTO "C21-703-7"."product" Values(nextval('pid_generator'),110,120,130,now(),1,60,30,20,50,5,slot_finder(5),10);
+INSERT INTO "C21-703-7"."product" Values(nextval('pid_generator'),300,300,300,now(),2,60,30,20,50,1,slot_finder(1),100);
+INSERT INTO "C21-703-7"."product" Values(nextval('pid_generator'),210,220,230,now(),3,80,50,40,70,2,slot_finder(2),37);
+INSERT INTO "C21-703-7"."product" Values(nextval('pid_generator'),70,50,30,now(),4,60,30,20,50,5,slot_finder(5),10);
+INSERT INTO "C21-703-7"."product" Values(nextval('pid_generator'),110,120,130,now(),5,60,30,20,50,5,slot_finder(5),70);
 
