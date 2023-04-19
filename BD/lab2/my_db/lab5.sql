3e09d77e82882c0fa19e0a0991d364cd2f3b7a5b
mmonastyrskiy <56514528+mmonastyrskiy@users.noreply.github.com>
2023-04-20 00:20:07 +0300

    lab5 start

---
+++ BD/lab2/my_db/lab5.sql
@@ -0,0 +1,38 @@
+--0
+
+CREATE OR REPLACE FUNCTION place_restrictions_trigger_func() RETURNS trigger as $$
+BEGIN
+if (SELECT spaces_left-1 from "C21-703-7"."Shelf" where shelf_id =new.shelf_id) < 0 then
+RAISE EXCEPTION 'Привышена емкость полки %', new.shelf_id;
+else
+UPDATE "C21-703-7"."Shelf" set spaces_left = spaces_left-1 where shelf_id = new.shelf_id
+END IF
+
+I
+
+
+
+
+END;
+$$ language plpgsql;
+CREATE TRIGGER place_restrictions_trigger BEFORE INSERT OR UPDATE ON "C21-703-7"."Shelf" s  LEFT JOIN "C21-703-7"."product" p 
+on(s.shelf_id = p.shelf_id)
+FOR EACH ROW
+EXECUTE PROCEDURE place_restrictions_trigger_func();
+
+
+
+--1
+CREATE OR REPLACE FUNCTION product_finder(client_n varchar(255),cdate date) RETURNS integer as $$
+BEGIN
+return SELECT count(product_id) from "C21-703-7"."product" p LEFT JOIN "C21-703-7"."Contract" c on(p.contract_id = c.contract_id)
+ JOIN "C21-703-7"."Client" cl on (cl.client_id = c.client_id)
+ where (client_name = client_n and expiration_date > cdate)
+ END;
+$$ language plpgsql;
+
+--2
+
+--3
+
+--4
